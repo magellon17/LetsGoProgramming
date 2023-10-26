410a292bcbb82fbc649c1ef4cc1fb5ffbac20e59
boris <borissiobko@gmail.com>
2023-10-26 15:39:01 +0300

    подметаю код

--- task1/cmd/main.go
+++ task1/cmd/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 	"task1/pkg/calc"
-	"task1/pkg/io"
+	util "task1/pkg/equationUtil"
 )
 
 func main() {
-	a := io.InputNumber("Введите первое число: ")
-	operator := io.ChooseOperator()
-	b := io.InputNumber("Введите второе число: ")
+	a := util.InputNumber("Введите первое число: ")
+	operator := util.ChooseOperator()
+	b := util.InputNumber("Введите второе число: ")
 
 	result := calc.Calculate(a, b, operator)
-	io.OutputResult(a, b, result, operator)
+	util.OutputResult(a, b, result, operator)
 }
