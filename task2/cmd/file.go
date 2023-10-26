ff4bacfc0610eeacb6cdad875ae2bec79da1531d
boris <borissiobko@gmail.com>
2023-10-26 20:52:00 +0300

    переписал функцию

--- task2/cmd/file.go
+++ task2/cmd/file.go
@@ -1,16 +1,41 @@
 package main
 
 func findKthLargest(nums []int, k int) int {
-	kthLargest := make([]int, k)
+	if k < 1 || k > len(nums) {
+		return -1 // Вернуть -1, если значение k находится за пределами допустимого диапазона
+	}
 
-	for i := 0; i < len(nums); i++ {
-		if nums[i] > kthLargest[k-1] {
-			for j := k - 1; j >= 1; j-- {
-				kthLargest[j] = kthLargest[j-1]
-			}
-			kthLargest[0] = nums[i]
+	return quickSelect(nums, 0, len(nums)-1, k)
+}
+
+func quickSelect(nums []int, left, right, k int) int {
+	if left == right {
+		return nums[left]
+	}
+
+	pivotIndex := partition(nums, left, right)
+	rank := pivotIndex - left + 1
+
+	if k == rank {
+		return nums[pivotIndex]
+	} else if k < rank {
+		return quickSelect(nums, left, pivotIndex-1, k)
+	} else {
+		return quickSelect(nums, pivotIndex+1, right, k-rank)
+	}
+}
+
+func partition(nums []int, left, right int) int {
+	pivot := nums[right]
+	i := left
+
+	for j := left; j < right; j++ {
+		if nums[j] >= pivot {
+			nums[i], nums[j] = nums[j], nums[i]
+			i++
 		}
 	}
 
-	return kthLargest[k-1]
+	nums[i], nums[right] = nums[right], nums[i]
+	return i
 }
