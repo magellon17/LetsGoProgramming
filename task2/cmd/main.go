bb4557c31dddda8b59767112ea2a62e0c8c9d3d4
boris <borissiobko@gmail.com>
2023-10-26 18:51:13 +0300

    Revert "дополнил readme && rnme arg in func"
    This reverts commit 3d1fa209c783ff3e96274154d320aaf17097a219.

--- task2/cmd/main.go
+++ task2/cmd/main.go
@@ -1,14 +1,14 @@
 package main
 
-func findKthLargest(nums []int, k int) int {
+func findKthLargest(arr []int, k int) int {
 	kthLargest := make([]int, k)
 
-	for i := 0; i < len(nums); i++ {
-		if nums[i] > kthLargest[k-1] {
+	for i := 0; i < len(arr); i++ {
+		if arr[i] > kthLargest[k-1] {
 			for j := k - 1; j >= 1; j-- {
 				kthLargest[j] = kthLargest[j-1]
 			}
-			kthLargest[0] = nums[i]
+			kthLargest[0] = arr[i]
 		}
 	}
 
